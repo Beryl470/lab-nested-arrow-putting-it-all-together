1362ae0f8e25af52f41d430930c9eb7744110a1a
beryl470 <khalaiberyl>
2025-12-01 15:30:11 +0300

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+function createLoginTracker(userInfo) {
+    let attemptCount = 0; // this must be OUTSIDE the attemptLogin function
 
+    const attemptLogin = (passwordAttempt) => {
+        attemptCount++;
 
+        if (attemptCount > 3) {
+            return "Account locked due to too many failed login attempts";
+        }
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        }
+
+        return `Attempt ${attemptCount}: Login failed`;
+    };
+
+    return attemptLogin;
+}
+
+module.exports = { createLoginTracker };
